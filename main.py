94151edb76aa0c15009f4b29637604876392ea24
Lyle Shaw <me@lyle.net.cn>
2023-03-29 21:56:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,4 @@ demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
 
 if __name__ == "__main__":
-    print("Running on 0.0.0.0:7007")
     demo.launch(server_name="0.0.0.0", server_port=7007, share=True)
