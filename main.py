7b7950a3278437911cb0a29488babd1d8e93fd89
Lyle Shaw <me@lyle.net.cn>
2023-05-27 11:42:50 +0800

    add img test

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import gradio as gr
+import torch
+import requests
+from torchvision import transforms
+
+model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
+response = requests.get("https://git.io/JJkYN")
+labels = response.text.split("\n")
 
 
-def greet(name):
-    return "Hello " + name + "!"
+def predict(inp):
+    inp = transforms.ToTensor()(inp).unsqueeze(0)
+    with torch.no_grad():
+        prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+        confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
+    return confidences
 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=predict,
+                    inputs=gr.inputs.Image(type="pil"),
+                    outputs=gr.outputs.Label(num_top_classes=3),
+                    examples=[["cheetah.jpg"]],
+                    )
 
+demo.launch()
 
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=7007, share=True)
